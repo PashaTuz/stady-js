ed99148ebb9128ddd794dd995422e92c969361d1
Pasha Tuz <90028614+PashaTuz@users.noreply.github.com>
2025-11-02 14:22:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let pasword = prompt('ведіть пароль:');
+const CORECT_PASSWORD = "1234"; 
+if (pasword === CORECT_PASSWORD) {
+    console.log('проходь');   
+} else {
+    console.log('не проходиш');
+    }
